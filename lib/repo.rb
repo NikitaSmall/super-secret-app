2ba91971cb2fe67404dd7359c724e739a53b0288
NikitaSmall <nikitasosnov@yahoo.com>
2016-03-19 15:32:44 +0200

    implement mongo indexes, preparation for parsing

---
+++ lib/repo.rb
@@ -0,0 +1,21 @@
+# this model stores parsed data from github api
+class Repo
+  include Mongoid::Document
+
+  field :full_name, type: String
+  field :original_query, type: String
+
+  field :html_url,  type: String
+  field :description, type: String
+
+  field :stargazers_count, type: Integer
+  field :commits_count, type: Integer
+  field :contributors_count, type: Integer
+
+  index({ full_name: 1 })
+  index({ original_query: 1 })
+
+  index({ stargazers_count: 1 })
+  index({ commits_count: 1 })
+  index({ contributors_count: 1 })
+end
